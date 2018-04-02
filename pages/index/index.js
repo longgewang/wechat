a29ca063951d9b65f19e3ab51812a0ae89a354ce
3210490980@qq.com <wsad12345>
2018-04-02 14:10:10 +0800

    设置slides的值

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,18 +1,21 @@
 // pages/index/index.js
+const app=getApp()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+    slides:null
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    this.setData({
+      slides:app.globalData.slides
+    })
   },
 
   /**
